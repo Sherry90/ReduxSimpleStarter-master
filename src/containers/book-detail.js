c4752e27351d45a93350f68970d9e4b0d9f7e61e
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-01 00:59:00 +0900

    lecture 48

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
     render() {
-        <div>Book Detail!</div>
+        return (
+            <div>Book Detail!</div>
+        );
     }
-
 }
+
+function mapStateToProps(state) {
+    return{
+        book: state.activeBook
+    };
+}
+
+export default connect(mapStateToProps)(BookDetail);
