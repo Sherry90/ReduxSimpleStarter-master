55de6d59459c738b0ad6df87c7bbb4ac01411fb9
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-09 23:56:14 +0900

    <SearchBar/>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"; // react 모듈을 불러와 React 변수에 할당
 import ReactDOM from 'react-dom';
+import SearchBar from "./components/search_bar";
 
 const API_KEY = 'AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI';
 
@@ -7,9 +8,13 @@ const API_KEY = 'AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI';
 // some HTML
 // AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI
 const App = () => { // using ES6 syntax
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'));
