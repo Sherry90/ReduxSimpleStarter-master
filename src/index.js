155b7c553497b009279018eb65c9ff63f253738c
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-05-02 17:11:01 +0900

    new react

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-import React from "react"; // react 모듈을 불러와 React 변수에 할당
-import ReactDOM from 'react-dom';
-import SearchBar from "./components/search_bar";
-
-const API_KEY = 'AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI';
-
 // Create a new component. This component should produce
 // some HTML
 // AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI
-const App = () => { // using ES6 syntax
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+
+const App = function () {
+    return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App/>, document.querySelector('.container'));
