b151c32ebdbc3aa8a0d5a7e27e555a5a61c3ace4
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-05-03 23:20:29 +0900

    this.state = {term : ''};

--- src/component/search_bar.js
+++ src/component/search_bar.js
@@ -1,6 +1,14 @@
 import React, {Component} from "react";
 
 class SearchBar extends Component {
+    constructor(props) { // js class가 실행될 때 가장 먼저 실행됨
+        super(props); // 부모클래스 Component에 정의된 메소드
+
+        this.state = {term : ''}; // 이 스테이트를 사용할 때마다 새로운 오브젝트를 생성하면서 초기화한다?
+        // term object에는 property가 포함되어있고 this.state에 저장된다.
+        // 유저가 search input에 값을 입력할 때마다 property term이 업데이트됨.
+        // term은 아무 이름이나 가능.
+    }
     render() {
         return <input onChange={(event) => console.log(event.target.value)} />; // onChange : react property 변경 event를 감지.
     }
