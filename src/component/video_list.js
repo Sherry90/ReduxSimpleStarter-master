9d013debf3a8556d8ecfd8ddc950c67282bd0d2d
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-05-09 17:23:25 +0900

    value = {this.state.term}

---
+++ src/component/video_list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem key={video.etag} video={video} />
+    });
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+            {/*video_list_item의 <li> 태그를 ul안에 배열로 그려준다*/}
+        </ul>
+    );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component}  from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from "./component/search_bar";
-
+import VideoList from "./component/video_list";
 const API_KEY = 'AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI';
 //npm install --save youtube-api-search
 
-// Create a new component. This component should produce
-// some HTML
-// AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI
+class App extends Component{
+    constructor(props) {
+        super(props);
+        //console.log(props);
+        this.state = { KeyVideos:[] };
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
+        //YTSearch({key: API_KEY, term: 'cat'}, (videos) => {
+        YTSearch({key: API_KEY, term: 'cat'}, (valVideos) => {
+            //console.log(videos);
+            //this.setState({ videos });
+            this.setState({ KeyVideos: valVideos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos = {this.state.KeyVideos} />
+            </div>
         );
+    }
 }
 
 // Take this component's generated HTML and put it
