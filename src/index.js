11e02d02c5ed877bd16ad1f3208ba3fdd29b9ca7
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-11-07 18:04:43 +0900

    function -> class YTsearch video[]

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from "react-dom";
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
-
 const API_KEY = "AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI";
 // npm install --save youtube-api-search
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component  {
+    constructor(props) {
+        super(props);
+
+        this.state = { video: [] };
+
+        // YTSearch p1. 구성옵션을 object로 넘김, p2 callback 함수
+        YTSearch({key : API_KEY, term : 'cat'}, (videoData) => {
+            this.setState({ videoData });
+            //this.setState({ videoData : videoData});
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
