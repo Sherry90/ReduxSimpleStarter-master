e69517424a8492020c1a71681b8a1458dce9be67
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-11-07 16:50:40 +0900

    react state component onChange setState

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,42 +1,26 @@
-import React, {Component} from "react";
+// import React from 'react';
+// class Sea rchBar extends React.Component {
+
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    constructor(props) { // js class가 실행될 때 가장 먼저 실행됨
-        super(props); // 부모클래스 Component에 정의된 메소드
+    constructor(props) {
+        super(props);
 
-        this.state = {term : ''}; // 이 스테이트를 사용할 때마다 새로운 오브젝트를 생성하면서 초기화한다?
-        // term object에는 property가 포함되어있고 this.state에 저장된다.
-        // 유저가 search input에 값을 입력할 때마다 property term이 업데이트됨.
-        // term은 아무 이름이나 가능.
+        this.state = {term : ''};
+        // constructor 내부에서 object 형태로 선언, 초기화
     }
-
     render() {
-        //return <input onChange={(event) => console.log(event.target.value)} />; // onChange : react property 변경 event를 감지.
         return (
-            <div className="search-bar">
-                <input
-                    value = {this.state.term}
-                    onChange={(event) => this.onInputChange(event.target.value)}/>
-                {/*// 건네고자 하는 새로운 state를 담은 객체*/}
-                {/*// constructor 안의 this.state는 this.setState으로 변경된다.*/}
+            <div>
+                value = {this.state.term}
+                <input onChange={event => this.setState({ term : event.target.value }) } />;
             </div>
         );
     }
-
-    /*
-    onInputChange(event) { // event 객체는 이벤트 발생에 대한 정보나 컨텍스트를 의미
-        console.log(event.target.value);
-        //console.log(event);
-    } // <input /> input event handler
-     */
-
-    onInputChange(term) {
-        this.setState({term});
-        this.props.onSearchTermChange(term);
-    }
-
-
-
+    // onChange는 input의 변화를 감지하는, react property.
+    // onChange ={}  {}내부에 이벤트 핸들러를 이용하여 이벤트 전달.
 }
+
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,17 @@
-import _ from 'lodash';
-import React, {Component}  from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from "./components/search_bar";
-import VideoList from "./components/video_list";
-const API_KEY = 'AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI';
-import VideoDetail from "./components/video_detail";
-//npm install --save youtube-api-search
+import React from 'react';
+import ReactDOM from "react-dom";
 
-class App extends Component{
-    constructor(props) {
-        super(props);
-        //console.log(props);
-        this.state = {
-            KeyVideos:[],
-            selectedVideo: null
+import SearchBar from './components/search_bar';
 
-        };
-        this.videoSearch('cat');
+const API_KEY = "AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI";
+// npm install --save youtube-api-search
 
-    }
-
-    videoSearch(term) {
-        //YTSearch({key: API_KEY, term: 'cat'}, (videos) => {
-        YTSearch({key: API_KEY, term: term}, (valVideos) => {
-            //console.log(videos);
-            //this.setState({ videos });
-            this.setState({
-                KeyVideos: valVideos,
-                selectedVideo: valVideos[0]
-            });
-        });
-    }
-
-    render() {
-        const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300)
-
-        return (
-            <div>
-                <SearchBar onSearchTermChange = {videoSearch} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList
-                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
-                    videos = {this.state.KeyVideos} />
-            </div>
-        );
-    }
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
