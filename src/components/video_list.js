e1ef4559eb8f9083d397cd62ae738e8ad3e84844
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-11-09 20:33:21 +0900

    const videoItems = props.videos.map((video) => {         return <VideoListItem key={video.etag} video = {video} />     });

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import VideoListItem from './video_list_item'
 const VideoList = (props) => {
-    console.log(props);
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem key={video.etag} video = {video} />
+    });
     return(
         <ul className="col-m-4 list-group">
-            {props.videos.length}
+            {videoItems}
         </ul>
     );
 }
