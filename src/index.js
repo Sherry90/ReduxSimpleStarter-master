a68b6c2ae4548ac5f5d90bf0d5eadef9db553563
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-11-14 20:20:45 +0900

    selectVideo setState

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
+import VideoDetail from "./components/video_detail";
+
 const API_KEY = "AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI";
 // npm install --save youtube-api-search
 
@@ -10,11 +12,17 @@ class App extends Component  {
     constructor(props) {
         super(props);
 
-        this.state = { videoData: [] };
+        this.state = {
+            videoData: [],
+            selectVideo : null
+        };
 
         // YTSearch p1. 구성옵션을 object로 넘김, p2 callback 함수
         YTSearch({key : API_KEY, term : 'cat'}, (videoData) => {
-            this.setState({ videoData });
+            this.setState({
+                videoData : videoData,
+                selectVideo : videoData[0]
+            });
             //this.setState({ videoData : videoData});
         });
     }
@@ -23,6 +31,7 @@ class App extends Component  {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video = {this.state.selectVideo} />
                 <VideoList videos = {this.state.videoData} />
             </div>
         );
