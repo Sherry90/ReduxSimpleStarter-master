2a74ce1c5d7281a13da6f57d280d7e9eb1c9575c
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-11-27 20:26:29 +0900

    make reducer

--- src/index.js
+++ src/index.js
@@ -1,50 +1,15 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
-import ReactDOM from "react-dom";
-import YTSearch from 'youtube-api-search'
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list'
-import VideoDetail from "./components/video_detail";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const API_KEY = "AIzaSyAR4yfD2CjCsoZtYvVyFZ04dOy45STHYgI";
-// npm install --save youtube-api-search
+import App from './components/app';
+import reducers from './reducers';
 
-class App extends Component  {
-    constructor(props) {
-        super(props);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-        this.state = {
-            videoData: [],
-            selectVideo : null
-        };
-
-        this.videoSearch('cat');
-    }
-
-    videoSearch(term) {
-        // YTSearch p1. 구성옵션을 object로 넘김, p2 callback 함수
-        YTSearch({key : API_KEY, term : term}, (videoData) => {
-            this.setState({
-                videoData : videoData,
-                selectVideo : videoData[0]
-            });
-        });
-    }
-
-    render() {
-        // debounce 함수는 입력한 ms 마다 호출된다
-        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
-        //console.log(this.state.videoData);
-        return (
-            <div>
-                <SearchBar onSearchTermChange ={videoSearch} />
-                <VideoDetail video = {this.state.selectVideo} />
-                <VideoList
-                    onVideoSelect = {selectVideo => this.setState({selectVideo})}
-                    videos = {this.state.videoData} />
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JS : ES6'},
+        { title: 'Harry'},
+        { title: 'Twin Tower'},
+        { title: 'Ruby'},
+    ]
+}
