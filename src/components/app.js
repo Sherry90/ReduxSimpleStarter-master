c53b32d15a91215e9834d0f0792684cce8250e64
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-12-12 20:39:53 +0900

    null process

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import BookList from "../conatiners/book-list";
+import BookDetail from "../conatiners/book-detail";
 
 export default class App extends Component {
     render() {
         return (
             <div>
                 <BookList />
+                <BookDetail />
             </div>
         );
     }
