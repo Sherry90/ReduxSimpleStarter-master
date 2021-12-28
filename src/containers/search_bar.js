4485767bb07aa4adb0068912a3a893a4dfe02c67
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-12-28 20:10:26 +0900

    submit preventDefault

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,33 @@
 import React, {Component} from "react";
-
+//wjddl_0226
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className="input-group">
-                <input />
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input
+                    placeholder="Search"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}/>
                 <span className="input-group-btn">
-                    <button type="submit" Class="btn btn-secondary">Submit</button>
+                    <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
             </form>
         );
