678618f560fb023c222eb7efdbb4784bfc09b86e
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2022-01-08 20:20:30 +0900

    ROOT_URL, install axios, install redux-promise,

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '0e57db31ee5f7768ee8751bbfea8b235';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export const fetchWeather = (city) => {
+    const url = `${ROOT_URL}&q=${city},kr`
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
